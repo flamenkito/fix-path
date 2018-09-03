2d56fc4a52bed877a200440dad75040538bf447a
Alejandro Ruso <alejandro.ruso.fx@gmail.com>
2018-09-03 12:39:29 +0300

    coverage

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,6 +4,6 @@ import { expect } from 'chai';
 describe('flamenkito-fix-path plugin, walk function', () => {
   it('should return dir list', async () => {
     const result = await walk('./src');
-    expect(result).to.deep.equal(['src/index.ts', 'src/test.ts']);
+    expect(result).to.deep.equal(['src/index.spec.ts', 'src/index.ts']);
   });
 });
